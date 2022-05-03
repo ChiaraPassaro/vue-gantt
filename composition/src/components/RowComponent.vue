9e0c3d42effdba4c395da12a2e0e26c1bbff3a50
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-05-03 09:44:35 +0200

    RowComponent

---
+++ composition/src/components/RowComponent.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import TaskComponent from "@/components/TaskComponent.vue";
+import { reactive } from "vue";
+const state = reactive({
+  task: {
+    width: 36,
+  },
+  row: {
+    height: 36,
+    margin: 0.2,
+  },
+});
+</script>
+
+<template>
+  <!--Task Row-->
+  <div
+    v-for="task in 3"
+    :key="task"
+    class="tasks__row__task"
+    :style="`height: ${state.row.height}px; margin: ${state.row.margin}em 0;`"
+  >
+    <TaskComponent :width="state.task.width"></TaskComponent>
+    <!--/Task Row-->
+  </div>
+</template>
+<style scoped lang="scss"></style>
--- composition/src/views/HomeView.vue
+++ composition/src/views/HomeView.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import TaskComponent from "@/components/TaskComponent.vue";
-import { reactive } from "vue";
-const state = reactive({
-  width: 36,
-});
+import RowComponent from "../components/RowComponent.vue";
 </script>
 
 <template>
   <main>
-    <button @click="state.width = state.width + 36">Add Width</button>
-    <TaskComponent :width="state.width"></TaskComponent>
+    <RowComponent />
   </main>
 </template>
 
